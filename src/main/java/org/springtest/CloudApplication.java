f4ca54886a96780d6260a04ef38f0ac576100895
Alexander Ryabokon <leonid555100@gmail.com>
2022-11-27 13:13:16 +0200

    Initial commit

---
+++ src/main/java/org/springtest/CloudApplication.java
@@ -0,0 +1,11 @@
+package org.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApplication.class, args);
+    }
+}
